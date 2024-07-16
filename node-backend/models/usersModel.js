83bd00e52a507e7f805db4e9d9b5f4d51a97cb7f
amxsupport <itmasterdesigne@gmail.com>
2024-07-16 22:56:11 +0000

    create models

---
+++ node-backend/models/usersModel.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose");
+
+const competitionSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+    },
+    email: {
+        type: String,
+    }
+});
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      default: "",
+    },
+    weight: {
+      type: Number,
+      default: 0,
+    },
+    height: {
+      type: Number,
+      default: 0,
+    },
+    stepGoal: {
+      type: Number,
+      default: 0,
+    },
+    calGoal: {
+      type: Number,
+      default: 0,
+    },
+    weightGoal: {
+      type: Number,
+      default: 0,
+    },
+    condition: {
+      type: String,
+      default: false,
+    },
+    veg: {
+      type: Boolean,
+      default: false,
+    },
+    vegan: {
+      type: Boolean,
+      default: false,
+    },
+    points: {
+      type: Number,
+      default: 0,
+    },
+    friend: {
+      type: Array,
+      default: [],
+    },
+    steps: {
+      type: Number,
+      default: 4149,
+    },
+    cals: {
+      type: Number,
+      default: 889,
+    },
+    badges: {
+      type: Array,
+      default: ["badge1", "badge2"],
+    },
+      competition: [competitionSchema]
+    },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("users", userSchema);
