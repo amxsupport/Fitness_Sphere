501fcfc7af100cd33823e46302424edb886c2a8b
amxsupport <itmasterdesigne@gmail.com>
2024-07-17 11:55:41 +0000

    create PoseModule.py

---
+++ fastapi-backend/PoseModule.py
@@ -0,0 +1,25 @@
+import cv2
+import mediapipe as mp
+import math
+
+class poseDetector() :
+    
+    def __init__(self, mode=False, complexity=1, smooth_landmarks=True,
+                 enable_segmentation=False, smooth_segmentation=True,
+                 detectionCon=0.5, trackCon=0.5):
+        
+        self.mode = mode 
+        self.complexity = complexity
+        self.smooth_landmarks = smooth_landmarks
+        self.enable_segmentation = enable_segmentation
+        self.smooth_segmentation = smooth_segmentation
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode, self.complexity, self.smooth_landmarks,
+                                     self.enable_segmentation, self.smooth_segmentation,
+                                     self.detectionCon, self.trackCon)
+
+
