ff4c7495019f2c044a073d23cf2a9ec82766c77e
amxsupport <itmasterdesigne@gmail.com>
2024-07-18 00:19:14 +0000

    update bicep_curl.py

--- fastapi-backend/bicep_curl.py
+++ fastapi-backend/bicep_curl.py
@@ -82,4 +82,93 @@ class BicepPoseAnalysis:
 
         return self.is_visible
 
+    def analyze_pose(
+        self,
+        landmarks,
+        frame,
+        results,
+        timestamp: int,
+        lean_back_error: bool = False,
+    ):
+        """Analyze angles of an arm for error detection
 
+        Args:
+            landmarks (): MediaPipe Pose landmarks
+            frame (): OpenCV frame
+            results (): MediaPipe Pose results
+            timestamp (int): timestamp of the frame
+            lean_back_error (bool, optional): If there is an lean back error detected, ignore the analysis. Defaults to False.
+
+        Returns:
+            _type_: _description_
+        """
+        has_error = False
+        self.get_joints(landmarks)
+
+        # Cancel calculation if visibility is poor
+        if not self.is_visible:
+            return (None, None, has_error)
+
+        # * Calculate curl angle for counter
+        bicep_curl_angle = int(calculate_angle(self.shoulder, self.elbow, self.wrist))
+        if bicep_curl_angle > self.stage_down_threshold:
+            self.stage = "down"
+        elif bicep_curl_angle < self.stage_up_threshold and self.stage == "down":
+            self.stage = "up"
+            self.counter += 1
+
+        # * Calculate the angle between the upper arm (shoulder & joint) and the Y axis
+        shoulder_projection = [
+            self.shoulder[0],
+            1,
+        ]  # Represent the projection of the shoulder to the X axis
+        ground_upper_arm_angle = int(
+            calculate_angle(self.elbow, self.shoulder, shoulder_projection)
+        )
+
+        # Stop analysis if lean back error is occur
+        if lean_back_error:
+            return (bicep_curl_angle, ground_upper_arm_angle, has_error)
+
+        # * Evaluation for LOOSE UPPER ARM error
+        if ground_upper_arm_angle > self.loose_upper_arm_angle_threshold:
+            has_error = True
+            self._extracted_from_analyze_pose_52(frame, "LOOSE UPPER ARM")
+            # Limit the saved frame
+            if not self.loose_upper_arm:
+                self.loose_upper_arm = True
+                self.detected_errors["LOOSE_UPPER_ARM"] += 1
+                results.append(
+                    {"stage": "loose upper arm", "frame": frame, "timestamp": timestamp}
+                )
+        else:
+            self.loose_upper_arm = False
+
+        # * Evaluate PEAK CONTRACTION error
+        if self.stage == "up" and bicep_curl_angle < self.peak_contraction_angle:
+            # Save peaked contraction every rep
+            self.peak_contraction_angle = bicep_curl_angle
+
+        elif self.stage == "down":
+            # * Evaluate if the peak is higher than the threshold if True, marked as an error then saved that frame
+            if (
+                self.peak_contraction_angle != 1000
+                and self.peak_contraction_angle >= self.peak_contraction_threshold
+            ):
+                self._extracted_from_analyze_pose_52(frame, "WEAK PEAK CONTRACTION")
+                self.detected_errors["PEAK_CONTRACTION"] += 1
+                results.append(
+                    {
+                        "stage": "peak contraction",
+                        "frame": frame,
+                        "timestamp": timestamp,
+                    }
+                )
+                has_error = True
+
+            # Reset params
+            self.peak_contraction_angle = 1000
+
+        return (bicep_curl_angle, ground_upper_arm_angle, has_error)
+
+    # TODO Rename this here and in `analyze_pose`
