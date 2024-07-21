b6c80d2b2e18a666b9e5ae2b9b84480edc3773bc
amxsupport <itmasterdesigne@gmail.com>
2024-07-21 21:34:10 +0000

    create staticData.js for testimonials

---
+++ frontend/src/staticData.js
@@ -0,0 +1,32 @@
+const testimonials = [
+  {
+    user: "amksa abdellaziz",
+    message: "fdkl ljfdlksf jldksjf jsdfkj klfjoisdj flkjflk j foij sfls",\
+    img: "",
+    designation: "Web Developer"
+  },
+  {
+    user: "amksa abdellaziz",
+    message: "fdkl ljfdlksf jldksjf jsdfkj klfjoisdj flkjflk j foij sfls",\
+    img: "",
+    designation: "Web Developer"
+  },
+  {
+    user: "amksa abdellaziz",
+    message: "fdkl ljfdlksf jldksjf jsdfkj klfjoisdj flkjflk j foij sfls",\
+    img: "",
+    designation: "Web Developer"
+  },
+]
+
+const users = [
+  {
+    name: "amksa abdellaziz",
+    email: "itmasterdesigne@gmail.com",
+    height: 181,
+    weight: 70,
+    
+  }
+]
+
+export { testimonials };
