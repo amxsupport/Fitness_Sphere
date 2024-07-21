e7879c9b02216017879893a3092b4609ab375ee1
amxsupport <itmasterdesigne@gmail.com>
2024-07-21 22:57:43 +0000

    create Footer2.jsx

---
+++ frontend/src/Base/Footer2.jsx
@@ -0,0 +1,10 @@
+import '/src/Home.css'
+
+export default function Footer() {
+  return (
+    <footer className="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-2b81"><div className="u-clearfix u-sheet u-sheet-1222">
+        <p className="u-small-text u-text u-text-variant u-text-1" onClick={()=>{
+      document.documentElement.scrollTop = 0;}}>Back to Top</p>
+      </div></footer>
+  )
+}
