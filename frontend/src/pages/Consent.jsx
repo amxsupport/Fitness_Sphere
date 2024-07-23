9b1874adf70afa72a03ff31eb09c43aead9174dd
amxsupport <itmasterdesigne@gmail.com>
2024-07-23 12:30:03 +0000

    create Consent.jsx

---
+++ frontend/src/pages/Consent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Consent = () => {
+    return (
+        <div className="w-screen h-screen">
+            <iframe
+                src="https://8b6b-2402-3a80-4190-beee-98ef-b30e-7fb3-6cb4.ngrok-free.app/static/images/video_20231028203253_1.jpg"
+                frameborder="0"
+                width={"100%"}
+                height="100%"
+            ></iframe>
+        </div>
+    );
+};
+
+export default Consent;
