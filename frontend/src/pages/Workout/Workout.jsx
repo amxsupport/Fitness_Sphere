9edef32c8c02ba64ddbe6f0ca0ed404d8d2d526b
amxsupport <itmasterdesigne@gmail.com>
2024-07-24 13:40:57 +0000

    update Workout.jsx

--- frontend/src/pages/Workout/Workout.jsx
+++ frontend/src/pages/Workout/Workout.jsx
@@ -141,4 +141,107 @@ const Workout = () => {
         stopRecording();
     };
 
+    // Capture a frame and send it to the backend
+    const captureAndSendFrame = () => {
+        if (capturing) {
+            const video = videoRef.current;
+            const canvas = canvasRef.current;
 
+            if (video && canvas) {
+                const context = canvas.getContext("2d");
+                if (!context) {
+                    return;
+                }
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+                console.log(canvas.width, canvas.height);
+                context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+                const image_base64 = canvas.toDataURL();
+
+                // Send the frame data to the FastAPI backend
+                try {
+                    // const params = new URLSearchParams({
+                    //     image_base64,
+                    // });
+                    fetch(
+                        // `${process.env.NEXT_PUBLIC_BACKEND_URL}/get_image?${params}`,
+                        `https://datahack-backend.onrender.com/api/get_image`,
+                        {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                                redirect: "follow",
+                            },
+                            body: JSON.stringify({ image_base64 }),
+                        }
+                    );
+                } catch (error) {
+                    if (error == "No face detected") {
+                        alert("No face detected!");
+                    }
+                    if (error == "More than one face detected") {
+                        alert("More than one face detected!");
+                    }
+                    console.error("Error sending frame data:", error);
+                }
+            }
+        }
+    };
+
+    return (
+        <div className="flex flex-col gap-2 w-full min-h-[80vh]">
+            <div className="flex-grow w-full flex flex-col justify-center items-center gap-4">
+                <div className="flex justify-center items-center gap-4 md:h-auto flex-col md:flex-row">
+                    {/* video of the user */}
+                    <video
+                        className="p-4 bg-gray-200 rounded-lg shadow-md"
+                        ref={videoRef}
+                        autoPlay={true}
+                        style={{ display: capturing ? "block" : "none" }}
+                    />
+
+                    {/* container to show and display questions and buttons like show answer and next question */}
+                    <div
+                        className="p-4 bg-gray-200 rounded-lg shadow-md h-full max-w-md"
+                        style={{ display: capturing ? "block" : "none" }}
+                    >
+                        <div className="flex flex-col justify-center items-center gap-4">
+                            <dotlottie-player
+                                autoplay
+                                loop
+                                mode="normal"
+                                src="/lottie/workout.lottie"
+                                style={{
+                                    width: "100%",
+                                }}
+                            ></dotlottie-player>
+                        </div>
+                    </div>
+                </div>
+
+                {/* contains buttons to start and stop the interview */}
+                <div className="flex justify-center items-center gap-8">
+                    <button
+                        className="p-4 bg-blue-500 text-white hover:bg-blue-600 hover:tracking-wider transition-all duration-300 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed"
+                        onClick={startInterview}
+                        disabled={capturing}
+                    >
+                        Start Exercise
+                    </button>
+
+                    <canvas ref={canvasRef} style={{ display: "none" }} />
+                    <button
+                        className="p-4 bg- text-white bg-red-500 hover:bg-red-600 hover:tracking-wider transition-all duration-300 rounded-lg disabled:opacity-75 disabled:cursor-not-allowed"
+                        onClick={stopInterview}
+                        disabled={!capturing}
+                    >
+                        Stop Exercise
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Workout;
